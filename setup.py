9afafd7090a48560ea8e4bdae50938142c2877a1
André Luiz de Amorim <streetomon@gmail.com>
2015-02-26 14:15:04 -0300

    Fixed f2py compilation.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from Cython.Build import cythonize
 from numpy.distutils.core import setup, Extension
 
 exts_c = cythonize(['example/*.pyx'])
-exts_f = [Extension(name='example/example_f2py',
+exts_f = [Extension(name='example.example_f2py',
                  sources=['example/example_f2py.f'])]
 setup(name='examples',
       version='0.1',
@@ -19,7 +19,7 @@ setup(name='examples',
       author_email='streetomon@gmail.com',
       license='MIT',
       packages=['example'],
-      ext_modules=exts_f, exts_c,
+      ext_modules=exts_f + exts_c,
       include_dirs=[numpy.get_include()],
       provides=['example'],
       requires=['numpy', 'cython'],
